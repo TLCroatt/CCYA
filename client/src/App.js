f89320ebfa407ba5ca1a5a45eeda1666f534bb26
Tyler Mathews <mrmathews08@gmail.com>
2020-11-14 12:51:28 -0700

    added Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Footer from "./components/Footer/Footer.js";
+import About from "./components/About";
+import Calendar from "./components/Calendar";
+import Container from "./components/Container";
+import Nav from "./components/Nav";
+import Registration from "./components/Registration";
+import Roster from "./components/Roster";
+import Sponsor from "./components/Sponsor"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        
+        <Route exact path="/" component={Home} />
+        <Footer />
+      
+      </div>
+    </Router>
+    
   );
 }
 
