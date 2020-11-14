5eec018ee361a80a2bb0cc68a1f344c5f59901df
Grant Pardington <grant.pardington@gmail.com>
2020-11-14 13:29:03 -0700

    Added functioning NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home"
+import { Container }from 'reactstrap';
+import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer.js";
 import About from "./components/About";
 import Calendar from "./components/Calendar";
-import Container from "./components/Container";
-import Nav from "./components/Nav";
+import Navigation from "./components/Nav/Nav";
 import Registration from "./components/Registration";
 import Roster from "./components/Roster";
-import Sponsor from "./components/Sponsor"
-
-
+import Sponsor from "./components/Sponsor";
 
 function App() {
   return (
     <Router>
       <div>
-        
-        <Route exact path="/" component={Home} />
+        <Navigation/> 
+        <Container>
+          <Route exact path="/" component={Home} /> 
+        </Container> 
         <Footer />
-      
       </div>
     </Router>
     
