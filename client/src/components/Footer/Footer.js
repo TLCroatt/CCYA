3e3e2444d2b68638922255fa3f3e8aeff8a2108a
Grant Pardington <grant.pardington@gmail.com>
2020-11-19 19:43:36 -0700

    Added row and columns to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Row, Col } from 'reactstrap';
 import "./style.css";
 
 export const Footer = (props) => {
   return (
     <footer className="text-center fixed-bottom footer">
-      <div className="card-footer">
+      <Row className="card-footer">
+        <Col sm='4'>
+        <small className="text-black mx-auto">
+         
+        </small>
+        </Col>
+        <Col sm='4'>
         <small className="text-black mx-auto">
           &copy; CCYA 2020 • All Rights Reserved
         </small>
-      </div>
+        </Col>
+        <Col sm='4'>
+        <small className="text-black mx-auto">
+          
+        </small>
+        </Col>
+      </Row>
     </footer>
   );
 }
