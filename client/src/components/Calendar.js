ea3f52b3fed510870971f5d24bd1cc38cc00bee8
frobbins <robbinsfatim@gmail.com>
2020-11-24 17:51:32 -0700

    switch to react-big calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,20 +1,37 @@
-// import React from 'react';
-// import Calendar from "react-calendar";
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-// export const ReactCalendar = () => {
-//   const [date, setDate] = useState(new Date());
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-//   const onChange = date => {
-//     setDate(date);
-//   };
+const localizer = momentLocalizer(moment);
 
-//   return (
-//     <div>
-//       <Calendar onChange={onChange} value={date} />
-//     </div>
-//   );
-// };
+class App extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
+      }
+    ]
+  };
 
-// render(<ReactCalendar />, document.querySelector("#root"));
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
 
-// export default Calendar;
+export default App;
