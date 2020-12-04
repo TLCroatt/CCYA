1423cbb4c79e496e0dec6bb21c237a11b4fe72f2
TLCroatt <trishacroatt@gmail.com>
2020-12-04 14:41:59 -0700

    working email form

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const mailRoutes = require('./mailRoutes');
 
 router.use('/users', userRoutes);
-router.use('/mail', mailRoutes);
 
 module.exports = router;
