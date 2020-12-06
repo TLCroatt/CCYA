7c735c95f665a59d73ff0ffe93b86c670dfe0418
TLCroatt <trishacroatt@gmail.com>
2020-12-06 15:32:04 -0700

    added media queries

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,20 +10,20 @@ import '../App.css';
 const items = [
   {
     src: './images/SB1.JPG',
-    altText: 'Slide 1',
-    caption: 'Slide 1',
+    altText: 'girl swinging bat',
+    caption: '',
     key: '1',
   },
   {
     src: './images/SB14.JPG',
-    altText: 'Slide 2',
-    caption: 'Slide 2',
+    altText: 'girl hitting ball with bat',
+    caption: '',
     key: '2',
   },
   {
     src: './images/SB3.JPG',
-    altText: 'Slide 3',
-    caption: 'Slide 3',
+    altText: 'girl pitching',
+    caption: '',
     key: '3',
   }
 ];
@@ -37,12 +37,10 @@ const Home = () => {
     <Container>
       <Row className="cards">
       <CardGroup>
-
         <Card className="card">
           <CardBody className="card-body">
             <CardTitle className="card-title" tag="h5">Mission Statement</CardTitle>
             <CardText className="card-text" id="cardText">Commerce City Youth Athletics believes that our programs help teach children about the importance of commitment, discipline, and teamwork. These skills will be beneficial to our participants throughout their adult lives. We believe our programs motivate our youths physically, to support good health, mentally, to enable them to think for themselves, and civically, to develop pride in our community and to represent our community in a positive manner. It also allows Commerce City residents to view competitive youth sports for free.</CardText>
-
           </CardBody>
           <CardImg className="card-img" top width="100%" src={ Logo } alt="CCYA Lady Eagles Logo" />
         </Card>
@@ -50,18 +48,16 @@ const Home = () => {
         <Card className="card">
           <CardImg className="card-img" top width="100%" src={ SB6 } alt="athlete tagging opposing athlete out as she approaches base" />
           <CardBody>
-
             <CardTitle className="card-title" tag="h5">Sponsors</CardTitle>
             <CardText className="card-text" id="cardText">We are grateful for the generosity of our partners! Thank you to our amazing sponsors.
-
             </CardText>
             <CardImg className="card-img" height="50%" width="100%" src={ sponsors3 } alt="Card image cap" />
           </CardBody>
         </Card>
       </CardGroup>
       </Row>
-      <Row id="about">
-        <Col>
+      <Row>
+        <Col className="about">
           <h1 id="about-header">About Us</h1>
           Commerce City Youth Athletics is a non-profit organization. We as a league are committed to honesty, integrity, and a commitment to provide low cost registration fees, so that as many children as possible may join the sport we provide. The Lady Eagles are a girls’ traveling softball team. We are able to provide this program through registration fees, fundraisers, and donations. All officers and coaches are non-paid volunteers.
           <br />
